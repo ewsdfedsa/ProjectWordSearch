98ef72f1ad79a1884fb361d8151cce36168d03c1
DESKTOP-VB5MA2I\ScorpionIPX <scorpionipx@gmail.com>
2018-01-02 20:16:16 +0200

    Matrice principala creata

--- WordSearch/WordSearch/Form1.Designer.cs
+++ WordSearch/WordSearch/Form1.Designer.cs
@@ -28,20 +28,45 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Joc nou";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 41);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(75, 20);
+            this.textBox1.TabIndex = 1;
             // 
             // WordSearch
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(780, 462);
+            this.ClientSize = new System.Drawing.Size(780, 487);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
             this.Name = "WordSearch";
             this.Text = "WordSearch";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- WordSearch/WordSearch/Form1.cs
+++ WordSearch/WordSearch/Form1.cs
@@ -12,9 +12,80 @@ namespace WordSearch
 {
     public partial class WordSearch : Form
     {
+        public TextBox[, ] matrice_joc;
+        public int dificultate_maxima;
         public WordSearch()
         {
+            this.dificultate_maxima = 20;
+            this.matrice_joc = new TextBox[dificultate_maxima, dificultate_maxima]; // maxim 20x20
             InitializeComponent();
+            this.initializare_interfata();
+
+        }
+
+        private void initializare_interfata()
+        {
+            // creare casute pentru litere
+            int dificultate_maxima = this.dificultate_maxima; // numar liniii si coloane
+            int dimensiune_casuta = 20;
+            int x_initial = 300;
+            int y_initial = 50;
+            int x;
+            int y;
+
+            for(int i=0; i< dificultate_maxima; i++)
+            {
+                for(int j=0; j< dificultate_maxima; j++)
+                {
+                    x = x_initial + (j * dimensiune_casuta);
+                    y = y_initial + (i * dimensiune_casuta);
+                    this.matrice_joc[i, j] = new TextBox();
+                    this.Controls.Add(this.matrice_joc[i, j]);
+                    this.matrice_joc[i, j].Size = new Size(dimensiune_casuta, dimensiune_casuta);
+                    this.matrice_joc[i, j].Location = new Point(x, y);
+                    this.matrice_joc[i, j].Text = "0";
+                    this.matrice_joc[i, j].TextAlign = HorizontalAlignment.Center;
+                    this.matrice_joc[i, j].Enabled = false;
+                    this.matrice_joc[i, j].Show();
+                }
+            }
+            /*
+            foreach(TextBox casuta in this.matrice_joc)
+            {
+                casuta.Show();
+                casuta.Location = new Point(15, 15);
+                casuta.Text = "0";
+            }*/
+        }
+
+        private void pregateste_joc()
+        {
+            int numar_de_linii = this.preia_numar_de_linii();
+            if(numar_de_linii == 0) // numar invalid de linii
+            {
+                return;
+            }
+        }
+
+        private int preia_numar_de_linii()
+        {
+            int numar_linii;
+            if (Int32.TryParse(textBox1.Text, out numar_linii) && numar_linii >= 5 && numar_linii <= 15)
+            {
+                Console.WriteLine("Se genereaza matrice patratica de {0}x{0}.", numar_linii.ToString());
+            }
+            else
+            {
+                string mesaj = "Eroare - numar de linii invalid!\nSe accepta doar valori numerice cuprinse intre 5 si 15!";
+                Console.WriteLine(mesaj);
+                numar_linii = 0;
+            }
+            return numar_linii;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.pregateste_joc();
         }
     }
 }
