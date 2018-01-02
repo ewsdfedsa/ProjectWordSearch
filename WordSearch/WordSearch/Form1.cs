0ac152584eef22d72d3be73b03275509225d99cd
DESKTOP-VB5MA2I\ScorpionIPX <scorpionipx@gmail.com>
2018-01-02 21:19:58 +0200

    adaugare cuvinte

--- WordSearch/WordSearch/Form1.cs
+++ WordSearch/WordSearch/Form1.cs
@@ -8,84 +8,131 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
 namespace WordSearch
 {
     public partial class WordSearch : Form
     {
         public TextBox[, ] matrice_joc;
         public int dificultate_maxima;
+        public int dificultate_minima;
+
+        public string[][][] cuvinte;
+        public string[][] limba_romana;
+        public string[] stiinta_ro;
+        public string[] medicina_ro;
+
         public WordSearch()
-        {
+        { 
             this.dificultate_maxima = 20;
+            this.dificultate_minima = 5;
+            this.initializare_cuvinte();
             this.matrice_joc = new TextBox[dificultate_maxima, dificultate_maxima]; // maxim 20x20
             InitializeComponent();
             this.initializare_interfata();
+            
+        }
 
+        private void initializare_cuvinte()
+        {
+            stiinta_ro = new string[] { "telescop", "computer", "multimetru", "senzor", "traductor", "pendul", "resort", "gravitatie" };
+            medicina_ro = new string[] { "analiza", "docotr", "medicament", "fiola", "seringa", "stetoscop", "microscop", "ambulanta" };
+     
+            limba_romana = new string[][] { stiinta_ro, medicina_ro };
+            cuvinte = new string[][][] { limba_romana };
+
+            Console.WriteLine(stiinta_ro[0].ToString());
+            Console.WriteLine(medicina_ro[0].ToString());
+            Console.WriteLine(limba_romana[0][2].ToString());
+            Console.WriteLine(cuvinte[0][0][2].ToString());
         }
 
         private void initializare_interfata()
         {
             // creare casute pentru litere
             int dificultate_maxima = this.dificultate_maxima; // numar liniii si coloane
-            int dimensiune_casuta = 20;
-            int x_initial = 300;
-            int y_initial = 50;
+            int dimensiune_casuta = 20; // dimensiunea fiecarei casute
+            int x_initial = 300; // pozitia primei casute
+            int y_initial = 50;  //
             int x;
             int y;
 
-            for(int i=0; i< dificultate_maxima; i++)
+            for(int i=0; i< dificultate_maxima; i++) // se creeaza matricea jocului
             {
                 for(int j=0; j< dificultate_maxima; j++)
                 {
-                    x = x_initial + (j * dimensiune_casuta);
-                    y = y_initial + (i * dimensiune_casuta);
-                    this.matrice_joc[i, j] = new TextBox();
-                    this.Controls.Add(this.matrice_joc[i, j]);
-                    this.matrice_joc[i, j].Size = new Size(dimensiune_casuta, dimensiune_casuta);
-                    this.matrice_joc[i, j].Location = new Point(x, y);
-                    this.matrice_joc[i, j].Text = "0";
-                    this.matrice_joc[i, j].TextAlign = HorizontalAlignment.Center;
+                    x = x_initial + (j * dimensiune_casuta); // se calculeaza coordonata x a casutei
+                    y = y_initial + (i * dimensiune_casuta); // se calculeaza coordonata y a casutei
+                    this.matrice_joc[i, j] = new TextBox(); // se creeaza o noua casuta
+                    this.Controls.Add(this.matrice_joc[i, j]); // se adauga casuta creata la forma existenta
+                    this.matrice_joc[i, j].Size = new Size(dimensiune_casuta, dimensiune_casuta); // se dimensioneaza
+                    this.matrice_joc[i, j].Location = new Point(x, y); /// se pozitioneaza
+                    this.matrice_joc[i, j].Text = "0"; // se scrie un text initial (optional)
+                    this.matrice_joc[i, j].TextAlign = HorizontalAlignment.Center; // se aliniaza textul pe centru
                     this.matrice_joc[i, j].Enabled = false;
-                    this.matrice_joc[i, j].Show();
+                    this.matrice_joc[i, j].Hide();
                 }
             }
-            /*
-            foreach(TextBox casuta in this.matrice_joc)
-            {
-                casuta.Show();
-                casuta.Location = new Point(15, 15);
-                casuta.Text = "0";
-            }*/
         }
 
         private void pregateste_joc()
         {
-            int numar_de_linii = this.preia_numar_de_linii();
-            if(numar_de_linii == 0) // numar invalid de linii
+            int dificultate = this.preia_dificultatea();
+            if(dificultate == 0) // numar invalid de linii
             {
                 return;
             }
+            this.pregateste_casutele(dificultate);
         }
 
-        private int preia_numar_de_linii()
+        private void reseteaza_casutele()
         {
-            int numar_linii;
-            if (Int32.TryParse(textBox1.Text, out numar_linii) && numar_linii >= 5 && numar_linii <= 15)
+            for (int i = 0; i < dificultate_maxima; i++)
             {
-                Console.WriteLine("Se genereaza matrice patratica de {0}x{0}.", numar_linii.ToString());
+                for (int j = 0; j < dificultate_maxima; j++)
+                {
+                    this.matrice_joc[i, j].Text = String.Empty;
+                    this.matrice_joc[i, j].Hide();
+                }
+            }
+        }
+
+        private void pregateste_casutele(int dificultate)
+        {
+            this.reseteaza_casutele();
+            for(int i=0; i<dificultate; i++)
+            {
+                for(int j=0; j<dificultate; j++)
+                {
+                    this.matrice_joc[i, j].Show();
+                }
+            }
+        }
+
+        private int preia_dificultatea()
+        {
+            int dificultate;
+            if (Int32.TryParse(textBox1.Text, out dificultate) && dificultate >= this.dificultate_minima && dificultate <= this.dificultate_maxima)
+            {
+                Console.WriteLine("Se genereaza matrice patratica de {0}x{0}.", dificultate.ToString());
             }
             else
             {
                 string mesaj = "Eroare - numar de linii invalid!\nSe accepta doar valori numerice cuprinse intre 5 si 15!";
                 Console.WriteLine(mesaj);
-                numar_linii = 0;
+                dificultate = 0;
             }
-            return numar_linii;
+            return dificultate;
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
             this.pregateste_joc();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
